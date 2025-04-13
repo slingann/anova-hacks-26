ac6cdbb2292b7fd6dcf2920dfff0ebe5d04c8bee
Robert Nochez <robertnochez@berkeley.edu>
2025-04-13 02:58:09 -0700

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Resources from './pages/Resources';
 import Projects from './pages/Projects';
 import Mentors from './pages/Mentors';
 import Stars from './components/Stars';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </BrowserRouter>
   );
 }
